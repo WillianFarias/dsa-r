6b332fd170da8eef7821f332f466c9034699cad1
WillianFarias <williansousafarias@gmail.com>
2021-08-04 09:32:04 -0300

    treemap e treemap customizado

--- Graficos.R
+++ Graficos.R
@@ -93,3 +93,37 @@ ggplot(data, aes(x=my_x, y=my_y))+
 ggplot(data, aes(x=my_x, y=my_y)) +
   geom_point(shape=1) + 
   geom_smooth(method = lm, color="red", se=TRUE) #se=FALSE) LIMITE DE ERRO
+
+
+#### Treemap ####
+install.packages("treemap")
+library(treemap)
+
+#Dados
+grupo = c(rep("grupo-1",4), rep("grupo-2",2), rep("grupo-3",3))
+subgrupo = paste("subgroup", c(1,2,3,4,1,2,1,2,3), sep = "-")
+valor = c(13,5,22,12,11,7,3,1,23)
+dados = data.frame(grupo, subgrupo, valor)
+View(dados)
+
+#Labels
+?treemap
+treemap(dados,
+        index = c("grupo", "subgrupo"),
+        vSize = "valor",
+        type = "index",
+        fontsize.labels = c(15,12),
+        fontcolor.labels = c("white", "orange"),
+        fontface.labels = c(2,1),
+        bg.labels = 220,
+        align.labels = list(c("center", "center"), c("right", "bottom")),
+        overlap.labels = 0.5,
+        inflate.labels = F)
+
+#Treem map customizado
+treemap(dados,
+        index = c("grupo", "subgrupo"),
+        vSize = "valor",
+        type = "index",
+        border.col = c("black", "white"),
+        border.lwds = c(7,2))
